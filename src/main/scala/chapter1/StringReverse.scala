fb5b79b8f57ac02773e8825fce8c35c6d816a769
Rebecca Mark <rebecca.lmark@gmail.com>
2018-03-10 21:18:29 -0800

    Added in place reverse implementation and test cases

--- src/main/scala/chapter1/StringReverse.scala
+++ src/main/scala/chapter1/StringReverse.scala
@@ -2,11 +2,11 @@ package chapter1
 
 object StringReverse {
   def reverse(arr: Array[Char]): Array[Char] = {
-    val length = arr.length
-
     if(arr == null) Array.empty
-    else if(length <= 1) arr
     else {
+      val length = arr.length
+      if(arr.length <= 1) return arr
+
       val newArr = Array.ofDim[Char](length)
       var i = length - 1
       var j = 0
@@ -19,6 +19,22 @@ object StringReverse {
       }
       newArr
     }
+  }
 
+  def inPlaceReverse(arr: Array[Char]): Array[Char] = {
+    if (arr == null || arr.length == 1) Array.empty
+    else {
+      var right = arr.length - 1
+      var left = 0
+
+      while (right > left){
+        val temp = arr(right)
+        arr(right) = arr(left)
+        arr(left) = temp
+        right -= 1
+        left += 1
+      }
+      arr
+    }
   }
 }
--- src/test/scala/chapter1/StringReverseSpec.scala
+++ src/test/scala/chapter1/StringReverseSpec.scala
@@ -10,6 +10,23 @@ class StringReverseSpec extends FlatSpec with Matchers {
   it should "work for empty string" in {
     val testArray: Array[Char] = Array()
     StringReverse.reverse(testArray) should contain theSameElementsAs Array[Char]()
-
+  }
+  it should "work for null input" in {
+    StringReverse.reverse(null) shouldBe Array.empty
+  }
+  "inPlaceReverse" should "reverse an odd C-style string in place" in {
+    val testArray: Array[Char] = Array('c', 'a', 't', 'c', 'h')
+    StringReverse.inPlaceReverse(testArray) should contain theSameElementsInOrderAs Array('h', 'c', 't', 'a', 'c')
+  }
+  it should "reverse an even C-style string in place" in {
+    val testArray: Array[Char] = Array('c', 'a', 't', 'c' )
+    StringReverse.inPlaceReverse(testArray) should contain theSameElementsInOrderAs Array( 'c', 't', 'a', 'c')
+  }
+  it should "work for empty string in place" in {
+    val testArray: Array[Char] = Array()
+    StringReverse.inPlaceReverse(testArray) should contain theSameElementsAs Array[Char]()
+  }
+  it should "work for null input in place" in {
+    StringReverse.inPlaceReverse(null) shouldBe Array.empty
   }
 }
