c87a305f2b888f72c973f13fdf73e4ff03b94391
Rebecca Mark <rebecca.lmark@gmail.com>
2018-08-22 19:38:15 -0700

    Lowest Common Ancestor tree practice implemented with specs

--- src/main/scala/tutoring/KthLeast.scala
+++ src/main/scala/tutoring/KthLeast.scala
@@ -2,6 +2,7 @@ package tutoring
 
 object KthLeast {
   def findKthLeast(k: Int, binaryTree: Tree[Int]): Int = {
+    // Could also first check to make sure k > 0 or k < root.count
     binaryTree match {
       case _:Empty => println(s"sadness $k | $binaryTree"); throw new IllegalStateException("Sad")
       case Node(data, count, l, r) if l.count + 1 == k => data
